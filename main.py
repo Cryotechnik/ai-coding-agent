62a39b4bc722887bcd4b23493e89f2e4af15f2e4
cryotechnik <watersguy8344@gmail.com>
2026-01-02 12:30:07 -0700

    B: connected gemini API

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from ai-coding-agent!")
+import os
+from google import genai
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+
+def main():
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        )
+    print(response.text)
 
 if __name__ == "__main__":
     main()
